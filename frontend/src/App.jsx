c87ce928493e6a0b916935b5123a03589a647173
Wiynxr <mkx.dake@gmail.com>
2023-03-21 01:40:12 +0300

    Начало разработки

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
