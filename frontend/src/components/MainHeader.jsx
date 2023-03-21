4fd4b888198df4e4e93e89f927cc50713a9dd222
Wiynxr <mkx.dake@gmail.com>
2023-03-21 23:37:28 +0300

    Наcтроил header

---
+++ frontend/src/components/MainHeader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/main-header.module.css";
+
+const MainHeader = () => {
+  return <div className={`${styles.wrapper} + bg-black text-white`}></div>;
+};
+
+export default MainHeader;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import MainHeader from "../components/MainHeader";
+import styles from "../styles/home.module.css";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className={`${styles.main}`}>
+      <MainHeader />
+    </div>
+  );
 };
 
 export default Home;
