b192cad0b76c1e0ac42f51790e48eb124ac7c124
Wiynxr <mkx.dake@gmail.com>
2023-03-21 18:18:48 +0300

    Обновил header добавил навигацию

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { AiOutlineUser } from "react-icons/ai";
 
 const Header = () => {
   return (
-    <header className="w-[100vw]">
-      <h1>GROZNY</h1>
+    <header className="w-[100vw] flex flex-col items-center">
+      <div className="w-[1200px] h-20 flex justify-between items-center">
+        <Link to="/">
+          <h1>Grozny</h1>
+        </Link>
+        <div className="w-[400px] flex justify-between items-center">
+          <NavLink to="/" activeClassName="active">
+            Главная
+          </NavLink>
+          <NavLink to="/discussion" activeClassName="active">
+            Обсуждение
+          </NavLink>
+          <NavLink to="/news" activeClassName="active">
+            Новости
+          </NavLink>
+          <NavLink to="/forum" activeClassName="active">
+            Форум
+          </NavLink>
+        </div>
+
+        <Link to="/user">
+          <AiOutlineUser className="text-2xl cursor-pointer" />
+        </Link>
+      </div>
     </header>
   );
 };
